8cdd04e7c2d13c15d4286958d0bede4569b62356
Haxend <45284621+Haxend@users.noreply.github.com>
2022-07-29 13:20:45 +0300

    Add files via upload

---
+++ Task1/Cleverence/Server.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace Server
+{
+    static class Server
+    {
+        private static ReaderWriterLock rwl = new ReaderWriterLock();
+        private static int count;
+
+        public static int GetCount()
+        {
+            rwl.AcquireReaderLock(10000);
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                rwl.ReleaseReaderLock();
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            rwl.AcquireWriterLock(10000);
+            try
+            {
+                count = value;
+            }
+            finally
+            {
+                Thread.Sleep(5000);
+                rwl.ReleaseWriterLock();
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    private static async Task HandleAsync()
+    {
+        Console.WriteLine("Enter Handler");
+        await Task.Delay(6000);
+        Console.WriteLine("Exit Handler");
+    }
+
+    static async Task Main(string[] args)
+    {
+        Task task = HandleAsync();
+        if (await Task.WhenAny(task, Task.Delay(5000)) == task)
+            Console.WriteLine("Completed successfully");
+        else
+            Console.WriteLine("Timeout occured");
+        Console.WriteLine("Done.");
+        Console.ReadKey();
+    }
+
+}
